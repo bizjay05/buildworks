f99df8d9ccb4f936946568dc8e30b4151289ab38
jay lim <bizjay0513@gmail.com>
2026-04-09 15:27:45 +0900

    다크모드

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className="dark" style={{ colorScheme: "dark" }}>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <WarningModal />
         <div className="flex min-h-screen">
